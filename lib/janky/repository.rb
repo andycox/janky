305f98cd192bfa8dd3fb51c9b7cc76f215c5136e
Andy Cox <acox@weather.com>
2014-05-15 13:36:41 -0400

    Git protocol is blocked, so use HTTPS always
    This may have some problems with https authentication, so we'll have to
    see whether we need to authenticate.

--- lib/janky/repository.rb
+++ lib/janky/repository.rb
@@ -24,7 +24,7 @@ module Janky
       repo = GitHub.repo_get(nwo)
       return if !repo
 
-      uri    = repo["private"] ? repo["ssh_url"] : repo["git_url"]
+      uri    = repo["clone_url"]
       name ||= repo["name"]
       uri.gsub!(/\.git$/, "")
 
